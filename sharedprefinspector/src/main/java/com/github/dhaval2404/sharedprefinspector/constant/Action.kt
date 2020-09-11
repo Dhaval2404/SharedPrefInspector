f46ec6bed3f2e1a0c06cf29766aea1dba564d060
Dhaval Patel <dhavalpatel244@gmail.com>
2020-09-11 22:56:09 +0530

    Added action

--- sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/constant/Action.kt
+++ sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/constant/Action.kt
@@ -3,12 +3,13 @@ package com.github.dhaval2404.sharedprefinspector.constant
 import androidx.annotation.StringDef
 
 @StringDef(Action.ADD, Action.UPDATE,
-    Action.DELETE)
+    Action.DELETE, Action.CLEAR)
 @Retention(AnnotationRetention.SOURCE)
 internal annotation class Action {
     companion object {
         const val ADD = "ADD"
         const val UPDATE = "UPDATE"
         const val DELETE = "DELETE"
+        const val CLEAR = "CLEAR"
     }
 }
