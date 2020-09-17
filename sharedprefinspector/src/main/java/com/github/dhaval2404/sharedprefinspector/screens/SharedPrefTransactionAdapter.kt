8d2193e18e92fd54c8590f5cf61f8b780dcff0be
Dhaval Patel <dhavalpatel244@gmail.com>
2020-09-17 23:48:56 +0530

    Show timestamp

--- sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/screens/SharedPrefTransactionAdapter.kt
+++ sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/screens/SharedPrefTransactionAdapter.kt
@@ -8,11 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.github.dhaval2404.sharedprefinspector.R
 import com.github.dhaval2404.sharedprefinspector.data.entity.SharedPref
 import kotlinx.android.synthetic.main.adapter_shared_pref_transaction.view.*
+import java.text.SimpleDateFormat
 
 class SharedPrefTransactionAdapter :
     RecyclerView.Adapter<SharedPrefTransactionAdapter.SharedPrefViewHolder>() {
 
     private val mSharedPrefTransactions = ArrayList<SharedPref>()
+    private val mSDF = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
 
     override fun getItemCount() = mSharedPrefTransactions.size
 
@@ -30,6 +32,7 @@ class SharedPrefTransactionAdapter :
         holder.actionTxt.text = sharedPref.action
         holder.keyTxt.text = sharedPref.key
         holder.valueTxt.text = sharedPref.value
+        holder.timestampTxt.text = mSDF.format(sharedPref.date)
         holder.valueTxt.isVisible = sharedPref.value != null
     }
 
@@ -37,6 +40,7 @@ class SharedPrefTransactionAdapter :
         internal val actionTxt = view.action
         internal val keyTxt = view.key
         internal val valueTxt = view.value
+        internal val timestampTxt = view.timestamp
     }
 
     fun refresh(list: List<SharedPref>) {
