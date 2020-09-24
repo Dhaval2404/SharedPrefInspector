c91702d7988c24ce8db14fcd212b3becc2c79fb0
Dhaval Patel <dhavalpatel244@gmail.com>
2020-09-24 23:53:50 +0530

    SharedPref Info Dialog added option to delete key

--- sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/screens/share_pref_listing/SharedPrefListingActivity.kt
+++ sharedprefinspector/src/main/java/com/github/dhaval2404/sharedprefinspector/screens/share_pref_listing/SharedPrefListingActivity.kt
@@ -1,36 +1,46 @@
 package com.github.dhaval2404.sharedprefinspector.screens.share_pref_listing
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.github.dhaval2404.sharedprefinspector.R
+import com.github.dhaval2404.sharedprefinspector.SharedPrefManager
 import kotlinx.android.synthetic.main.activity_shared_pref_listing.*
 import kotlinx.android.synthetic.main.dialog_shared_pref_info.view.*
 
 class SharedPrefListingActivity : AppCompatActivity(R.layout.activity_shared_pref_listing) {
+
+    private lateinit var mSharedPrefManager: SharedPrefManager
+    private lateinit var mSharedPreferences: SharedPreferences
+    private lateinit var mSharedPrefListingAdapter: SharedPrefListingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val sharePref = intent.getStringExtra("shared_pref")
 
-        val sharedPref = getSharedPreferences(sharePref, Context.MODE_PRIVATE)
-        val sharedPrefs: List<Pair<String, Any?>> =
-            sharedPref.all.map { it.key to it.value }.toList()
+        mSharedPrefManager = SharedPrefManager(this, sharePref!!)
+        mSharedPreferences= getSharedPreferences(sharePref, Context.MODE_PRIVATE)
 
-        val adapter = SharedPrefListingAdapter()
-        adapter.setItemClickListener {
+        mSharedPrefListingAdapter = SharedPrefListingAdapter()
+        mSharedPrefListingAdapter.setItemClickListener {
             showSharePrefInfo(it)
         }
-        sharedPrefRV.adapter = adapter
+        sharedPrefRV.adapter = mSharedPrefListingAdapter
 
-        adapter.refresh(sharedPrefs)
+        refresh()
     }
 
-    private fun showSharePrefInfo(pair: Pair<String, Any?>){
+    private fun refresh(){
+        val sharedPrefs: List<Pair<String, Any?>> =
+            mSharedPreferences.all.map { it.key to it.value }.toList()
+        mSharedPrefListingAdapter.refresh(sharedPrefs)
+    }
+
+    private fun showSharePrefInfo(pair: Pair<String, Any?>) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("SharePref Info")
 
@@ -41,6 +51,17 @@ class SharedPrefListingActivity : AppCompatActivity(R.layout.activity_shared_pre
         view.keyEt.setText(pair.first)
         pair.second?.let {
             view.valueEt.setText(it.toString())
+        }
+
+        builder.setPositiveButton("Update") { _, _ ->
+
+        }
+
+        builder.setNegativeButton("Cancel", null)
+
+        builder.setNeutralButton("Delete") { _, _ ->
+            mSharedPrefManager.remove(pair.first)
+            refresh()
         }
 
         val alertDialog = builder.create()
